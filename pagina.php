ac5eaa6a270f75a4482524b69714a5d176648514
Gilmar Zezilia Alves <gilmaralves914@gmail.com>
2020-11-25 22:52:33 -0300

    Adicionando arquivo do Bootstrap
    Tive que baixar dois arquivos do  Bootstrap, pois o mesmo não estava funcionando pelo link.

--- pagina.php
+++ pagina.php
@@ -6,19 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, 
 minimum-scale=1, width=device-width" />
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Trispace:wght@100&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="mobileui/style.css" />
-    <link rel="stylesheet" type="text/css" href="css/pagina.css" />
+     <link href="https://fonts.googleapis.com/css2?family=Trispace:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="mobileui/style.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="css/pagina.css" rel="stylesheet"/>
     <title>Pagina Inicial teste</title>
 </head>
 
 <body>
 
-    <div id="menu" class="row text-center ">
+<div id="menu" class="row text-center ">
         <div class="col-4">
-            <a href=""><img src="img/home.svg" class="icons"></a>
+            <a href="#"><img src="img/home.svg" class="icons"></a>
         </div>
         <div class="col-4 ">
             <a href="chat.php"><img src="img/chat.svg" class="icons"></a>
@@ -28,6 +28,7 @@ minimum-scale=1, width=device-width" />
             <a href="profissionais.php"><img src="img/quemsomos.svg" class="icons"></a>
         </div>
     </div>
+
 
 
     <div class="conteudo">
@@ -101,10 +102,8 @@ minimum-scale=1, width=device-width" />
 
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+    
+<script src="js/bootstrap.min.js" ></script>
 </body>
 
 </html>
